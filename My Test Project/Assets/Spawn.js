73033d14449effb5a11728f79190dc0e1d854049
Avelina Kim <avelina@gmail.com>
2014-08-10 16:06:32 -0700

    Spawn pieces

---
+++ My Test Project/Assets/Spawn.js
@@ -0,0 +1,13 @@
+﻿#pragma strict
+
+var Piece = GameObject();
+var spawnPos = Vector2();
+private var spawnRot = Quaternion();
+
+function Start () {
+
+}
+
+function Update () {
+	Instantiate(Piece, spawnPos, spawnRot);
+}
