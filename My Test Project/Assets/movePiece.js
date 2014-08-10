aa5221e72a70ae1b8d4f9d342425dec81375cb9e
Avelina Kim <avelina@gmail.com>
2014-08-10 15:55:55 -0700

    responds to A, D, Space

---
+++ My Test Project/Assets/movePiece.js
@@ -0,0 +1,26 @@
+﻿#pragma strict
+
+var sideNudge = Vector2(0.2, 0);
+
+function Start () {
+
+}
+
+function Update () {
+	if(Input.GetKeyDown(KeyCode.A)) {
+		rigidbody2D.position -= sideNudge;
+		
+	}
+	
+	if(Input.GetKeyDown(KeyCode.D)) {
+		rigidbody2D.position += sideNudge;
+		
+	}
+	
+	if(Input.GetKeyDown(KeyCode.Space)) {
+		rigidbody2D.rotation -= 90;
+		
+	}
+}
+
+			//Debug.Log(rigidbody2D.position);
